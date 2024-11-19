576e125bf48c3dbd6caa4fb7ce3e5b0ddbff6aae
Kobus Goosen <kgoosen@videologyinc.com>
2024-11-19 13:08:02 +0000

    fixup

--- vdlg_lvds/set_res.py
+++ vdlg_lvds/set_res.py
@@ -31,30 +31,32 @@ brands = {
 
 def poll_command(serial_device, command, retries=2, delay=0.1):
     for _ in range(retries):
-        response = serial_device.transceive(bytearray.fromhex(command))
-        if b"9041ff" in response:
+        response = serial_device.transceive(bytearray.fromhex(command)).hex()
+        if "9041ff" in response:
             return response
         sleep(delay)
     raise RuntimeError(f"Failed to execute command: {command}")
 
 def detect_camera_brand(serial_device):
-    response = serial_device.transceive(bytearray.fromhex("81090002FF"))
+    response = serial_device.transceive(bytearray.fromhex("81090002FF")).hex()
     for brand, code in brands.items():
         if code in response:
+            print(f"Camera ID response: {response}.\nDetected {brand.upper()} zoomblock")
             return brand
     raise ValueError(f"Unknown camera: {response}")
 
 def set_resolution(serial_device, resolution, brand):
-    if brand.tolower() not in resolution_commands:
+    if brand.lower() not in resolution_commands:
         raise ValueError(f"Unsupported camera: {brand}")
     else:
-        if resolution not in resolution_commands[brand]:
+        if resolution not in resolution_commands[brand.lower()]:
             raise ValueError(f"Unsupported resolution: {resolution}")
         else:
             commands = resolution_commands[brand].get(resolution)
             for command in commands:
                 retry = 20 if "1041903" in command else 2
-                poll_command(serial_device, command, retries=retry)
+                res = poll_command(serial_device, command, retries=retry)
+                print(f"cmd: {command}, res: {res}")
 
 def main():
     parser = argparse.ArgumentParser(description="Set camera resolution via LvdsSerial")
